dc44df53138266ac31b2423acd0a21b30c1b9f83
the3dsandwich <duhweiwei1013@gmail.com>
2018-04-22 02:10:33 +0800

    group discussion page

--- src/Components/user/Timetable.js
+++ src/Components/user/Timetable.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Segment } from "semantic-ui-react";
-import TimetableView from "./TimetableView";
+// import TimetableView from "./TimetableView";
 
 export default class Timetable extends Component {
   constructor(props) {
--- src/Components/user/TimetableView.js
+++ src/Components/user/TimetableView.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Segment, Table } from "semantic-ui-react";
+import { Table } from "semantic-ui-react";
 
 export default class GroupDiscussion extends Component {
   constructor(props) {
